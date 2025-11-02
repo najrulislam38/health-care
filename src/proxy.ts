f18c516c98ab9b8cfc9b1c0473b3b49c1ca219b5
Najrul Islam <mdnajrulislam.4738@gmail.com>
2025-11-02 14:53:20 +0600

    modified proxy file and get user from accesstoken and refreshtoken

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,34 +1,111 @@
 import { NextResponse } from "next/server";
-import type { NextRequest } from "next/server";
+import { NextRequest } from "next/server";
+import { jwtDecode } from "jwt-decode";
+
+interface IUser {
+  id: string;
+  email: string;
+  role: "ADMIN" | "DOCTOR" | "PATIENT";
+  exp: number;
+  iat: number;
+}
+
+const roleBaseRoutes = {
+  ADMIN: ["/admin/dashboard"],
+  DOCTOR: ["/doctor/dashboard"],
+  PATIENT: [
+    "/patient/dashboard",
+    "/patient/appointments",
+    "/patient/medical-records",
+  ],
+};
+
+const authRoutes = ["/login", "/register", "/forgot-password"];
 
 // This function can be marked `async` if using `await` inside
-export function proxy(request: NextRequest) {
-  const token = request.cookies.get("accessToken")?.value;
+export async function proxy(request: NextRequest) {
+  const accessToken = request.cookies.get("accessToken")?.value;
+
+  const refreshToken = request.cookies.get("refreshToken")?.value;
 
   const { pathname } = request.nextUrl;
 
-  const protectedPaths = [
-    "/dashboard",
-    "/profile",
-    "/settings",
-    "/appointments",
-  ];
-
-  const authRoutes = ["/login", "/register", "/forgot-password"];
-
-  const isProtectedPath = protectedPaths.some((path) =>
-    pathname.startsWith(path)
-  );
-
-  const isAuthRoute = authRoutes.some((path) => pathname === path);
-
-  if (isProtectedPath && !token) {
-    return NextResponse.redirect(new URL("/login", request.url));
+  if (!accessToken && !refreshToken && !authRoutes.includes(pathname)) {
+    return NextResponse.redirect(
+      new URL(`/login?redirect=${pathname}`, request.url)
+    );
   }
 
-  if (isAuthRoute && token) {
-    return NextResponse.redirect(new URL("/", request.url));
+  let user: IUser | null = null;
+
+  try {
+    if (accessToken) {
+      user = jwtDecode(accessToken);
+    }
+  } catch (error: any) {
+    console.log("error decoding access token:", error.message);
+    return NextResponse.redirect(
+      new URL(`/login?redirect=${pathname}`, request.url)
+    );
   }
+
+  if (!user && refreshToken) {
+    try {
+      const refresher = await fetch(
+        `${process.env.NEXT_PUBLIC_API_URL}/auth/refresh`,
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ refreshToken }),
+        }
+      );
+
+      if (refresher.ok) {
+        const newAccessToken = request.cookies.get("accessToken")?.value;
+        user = jwtDecode(newAccessToken!);
+
+        // return NextResponse.redirect(new URL(pathname, request.url));
+
+        return NextResponse.next();
+      } else {
+        const response = NextResponse.redirect(
+          new URL(`/login?redirect=${pathname}`, request.url)
+        );
+        response.cookies.delete("accessToken");
+        response.cookies.delete("refreshToken");
+      }
+    } catch (error) {
+      console.log("error decoding refresh token:", error);
+      const response = NextResponse.redirect(
+        new URL(`/login?redirect=${pathname}`, request.url)
+      );
+      response.cookies.delete("accessToken");
+      response.cookies.delete("refreshToken");
+      return response;
+    }
+  }
+
+  if (user) {
+    const allowedRoutes = user ? roleBaseRoutes[user.role] : [];
+    if (allowedRoutes && allowedRoutes.some((r) => pathname.startsWith(r))) {
+      return NextResponse.next();
+    } else {
+      return NextResponse.redirect(new URL("/unauthorized", request.url));
+    }
+  }
+
+  if (user && authRoutes.includes(pathname)) {
+    return NextResponse.redirect(new URL("/"));
+  }
+
+  // const protectedPaths = [
+  //   "/dashboard",
+  //   "/profile",
+  //   "/settings",
+  //   "/appointments",
+  // ];
 
   return NextResponse.next();
 }
